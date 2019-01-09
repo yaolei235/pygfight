8aebb7b6346388432c38174b7cf55d0387dcb557
Nealva <401586573@qq.com>
2019-01-09 15:52:29 +0800

    修改订单接口添加分页查询方法

--- interface/src/main/java/cn/itcast/core/service/OrderService.java
+++ interface/src/main/java/cn/itcast/core/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.core.service;
 
+import cn.itcast.core.pojo.entity.PageResult;
 import cn.itcast.core.pojo.entity.Result;
 import cn.itcast.core.pojo.log.PayLog;
 import cn.itcast.core.pojo.order.Order;
@@ -12,4 +13,6 @@ public interface OrderService {
     public PayLog getPayLogByUserName(String userName);
 
     public void updatePayStatus(String userName);
+
+    PageResult findPageBySellerId(Order order, int page, int rows, String sellerId);
 }
