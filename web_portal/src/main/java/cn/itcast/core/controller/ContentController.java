9d043ed0d1eb944b50bc484256e63c160d603357
jiawei <315448654@qq.com>
2019-01-11 17:55:21 +0800

    商品分类

--- web_portal/src/main/java/cn/itcast/core/controller/ContentController.java
+++ web_portal/src/main/java/cn/itcast/core/controller/ContentController.java
@@ -1,7 +1,9 @@
 package cn.itcast.core.controller;
 
 import cn.itcast.core.pojo.ad.Content;
+import cn.itcast.core.pojo.item.ItemCat;
 import cn.itcast.core.service.ContentService;
+import cn.itcast.core.service.ItemCatService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,9 @@ public class ContentController {
     @Reference
     private ContentService contentService;
 
+    @Reference
+    private ItemCatService itemCatService;
+
     /**
      * 根据分类id查询广告集合
      * @param categoryId
@@ -27,4 +32,15 @@ public class ContentController {
     public List<Content> findByCategoryId(Long categoryId) {
         return contentService.findByCategoryIdFromRedis(categoryId);
     }
+
+    /**
+     * 商品分类
+     * @return
+     */
+    @RequestMapping("/showItemCat")
+    public List<ItemCat> showItemCat(){
+        List<ItemCat> itemCatList = itemCatService.findItemCatList();
+        return itemCatList;
+    }
+
 }
