c21d255966cc24c90cbdee5d3ee73c690ae2ed6b
yaolei <2350596325@qq.com>
2019-01-11 09:53:24 +0800

    修改认证类

--- web_portal/src/main/java/cn/itcast/core/service/UserDetailServiceImpl.java
+++ web_portal/src/main/java/cn/itcast/core/service/UserDetailServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.itcast.core.service;
 
 import cn.itcast.core.dao.user.UserDao;
+import cn.itcast.core.pojo.user.UserQuery;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.GrantedAuthority;
@@ -29,6 +30,24 @@ public class UserDetailServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //定义权限集合
         List<GrantedAuthority> authList = new ArrayList<>();
+        
+        //用户登录时候,经验值加1
+        if (username!=null){
+            UserQuery query = new UserQuery();
+            UserQuery.Criteria criteria = query.createCriteria();
+            criteria.andUsernameEqualTo(username);
+
+            //获取该用户民对应的用户(列表)
+            List<cn.itcast.core.pojo.user.User> userList = userDao.selectByExample(query);
+
+            for (cn.itcast.core.pojo.user.User user : userList) {
+                //经验值加1
+                user.setExperienceValue(user.getExperienceValue()+1);
+                //更新到数据库
+                userDao.updateByPrimaryKeySelective(user);
+            }
+        }
+
         //向权限集合中加入访问权限
         authList.add(new SimpleGrantedAuthority("ROLE_USER"));
 
